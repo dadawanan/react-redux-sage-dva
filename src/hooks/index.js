0e7f6fb0f39740c689013b360556e0c463e67c87
dadawanan <h1159827456@outlook.com>
2019-11-26 01:21:08 +0800

    hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -6,8 +6,12 @@
 
 /**
  * 1.建立一个包裹组件，通过children的方式去渲染
- * 2.新建reducer,编写异步,children组件中使用reducer
- * 3.
+ * 2.在父组件中用useReducer初始化state,dispatch
+ * 3.给createContext的provider赋值value
+ * 3.5.把子组件赋值给children(包裹一层，防止重复渲染)
+ * 4.配置reducer,处理不同逻辑
+ * 5.在子组件中接收useContext的值(state,dispatch),传入子组件
+ * 6.子子组件中使用state中的值，并dispatch事件
  */
 import React, { useContext, createContext, useReducer } from 'react';
 const NumContext = createContext();
